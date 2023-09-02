3284447d3809d45a56db00b3112c00c1b6c0a52b
TheSketty <roma.baygushev@mail.ru>
2023-09-02 21:54:58 +0300

    bomzh code

---
+++ tetris/index.js
@@ -0,0 +1,14 @@
+let columns = 10;
+let rows = 20;
+
+let gameField = [];
+for (let i = 0; i < rows; i++) {
+    gameField[i] = [];
+    for (let j = 0; j < columns; j++) {
+        gameField[i][j] = 0;
+    }
+}
+
+let blocks = [
+    
+]
