262a36cbc6701826f52f95624b351c2beeded12d
TheSketty <roma.baygushev@mail.ru>
2024-03-31 13:19:50 +0300

    adjust

--- 3d render/main.js
+++ 3d render/main.js
@@ -4,23 +4,29 @@ const width = document.body.clientWidth
 const height = document.body.clientHeight
 canvas.setAttribute("width", width)
 canvas.setAttribute("height", height)
-// const fps = window.request
 
-let camera = [600, 0, 0]
-let F = 600
+// let camera = [600, 0, 0]
+let size
+if (width > height) {
+    size = height / 5
+}
+else {
+    size = width / 5
+}
+let F = size * 3
 let cube = [
-    [-200, -200, -200],
-    [200, -200, -200],
-    [-200, 200, -200],
-    [200, 200, -200],
-    [-200, -200, 200],
-    [200, -200, 200],
-    [-200, 200, 200],
-    [200, 200, 200]
+    [-size, -size, -size],
+    [size, -size, -size],
+    [-size, size, -size],
+    [size, size, -size],
+    [-size, -size, size],
+    [size, -size, size],
+    [-size, size, size],
+    [size, size, size]
 ]
 
 function drawLine(coord1, coord2) {
-    ctx.lineWidth = 5
+    ctx.lineWidth = size / 40
     ctx.beginPath()
     ctx.moveTo(width / 2 + coord1[0], height / 2 + coord1[1])
     ctx.lineTo(width / 2 + coord2[0], height / 2 + coord2[1])
@@ -35,16 +41,14 @@ function rotateY(mat, angle) {
     return [mat[0] * rot[0][0] + mat[2] * rot[0][1], mat[1], mat[0] * rot[1][0] + mat[2] * rot[1][1]]
 }
 
-let theta = 0;
 function draw () {
     ctx.clearRect(0, 0, width, height)
     let dots = []
     for (var i in cube) {
-        let cubeRot = rotateY(cube[i], theta)
+        let cubeRot = rotateY(cube[i], Date.now() * Math.PI / 180 / 16)
         let x = F * cubeRot[0] / (F + cubeRot[2])
         let y = F * cubeRot[1] / (F + cubeRot[2])
         dots.push([x, y])
-        theta += Math.PI / 180 / 16
     }
     drawLine(dots[0], dots[1])
     drawLine(dots[0], dots[2])
