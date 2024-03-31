9ef2dc1b42a9e84f3d75b8551b9585a746cbda6d
TheSketty <roma.baygushev@mail.ru>
2024-03-31 13:04:08 +0300

    I MADE IT!!!!

--- 3d render/main.js
+++ 3d render/main.js
@@ -1,10 +1,5 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-ctx.lineWidth = 200
-ctx.beginPath()
-ctx.moveTo(0, 0)
-ctx.lineTo(1000, 1000)
-ctx.stroke()
 const width = document.body.clientWidth
 const height = document.body.clientHeight
 canvas.setAttribute("width", width)
@@ -25,11 +20,11 @@ let cube = [
 ]
 
 function drawLine(coord1, coord2) {
+    ctx.lineWidth = 5
     ctx.beginPath()
-    ctx.moveTo(coord1[0], coord1[2])
-    ctx.lineTo(coord2[0], coord2[1])
+    ctx.moveTo(width / 2 + coord1[0], height / 2 + coord1[1])
+    ctx.lineTo(width / 2 + coord2[0], height / 2 + coord2[1])
     ctx.stroke()
-    // console.log("line")
 }
 
 function rotateY(mat, angle) {
@@ -40,19 +35,29 @@ function rotateY(mat, angle) {
     return [mat[0] * rot[0][0] + mat[2] * rot[0][1], mat[1], mat[0] * rot[1][0] + mat[2] * rot[1][1]]
 }
 
-// let theta = 0;
-// function draw () {
-//     ctx.clearRect(0, 0, width, height)
-//     let dots = []
-//     for (var i in cube) {
-//         let cubeRot = rotateY(cube[i], theta)
-//         let x = F * cubeRot[0] / (F + cubeRot[2])
-//         let y = F * cubeRot[1] / (F + cubeRot[2])
-//         dots.push([x, y])
-//         ctx.fillRect(width / 2 + x, height / 2 + y, 10, 10)
-//         theta += Math.PI / 180 / 16
-//     }
-//     drawLine(dots[0], dots[1])
-//     requestAnimationFrame(draw)
-// }
-// requestAnimationFrame(draw)
+let theta = 0;
+function draw () {
+    ctx.clearRect(0, 0, width, height)
+    let dots = []
+    for (var i in cube) {
+        let cubeRot = rotateY(cube[i], theta)
+        let x = F * cubeRot[0] / (F + cubeRot[2])
+        let y = F * cubeRot[1] / (F + cubeRot[2])
+        dots.push([x, y])
+        theta += Math.PI / 180 / 16
+    }
+    drawLine(dots[0], dots[1])
+    drawLine(dots[0], dots[2])
+    drawLine(dots[0], dots[4])
+    drawLine(dots[1], dots[3])
+    drawLine(dots[1], dots[5])
+    drawLine(dots[2], dots[3])
+    drawLine(dots[2], dots[6])
+    drawLine(dots[3], dots[7])
+    drawLine(dots[4], dots[5])
+    drawLine(dots[4], dots[6])
+    drawLine(dots[5], dots[7])
+    drawLine(dots[6], dots[7])
+    requestAnimationFrame(draw)
+}
+requestAnimationFrame(draw)
